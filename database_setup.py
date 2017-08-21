9adbaaf8b9d3b29026e70938bf506a1aa857832a
tstillwell2k17 <tristanstillwell@gmail.com>
2017-08-20 22:22:35 -0400

    Fixed formatting/spacing & made main.py (mostly) PEP8 compliant

--- database_setup.py
+++ database_setup.py
@@ -11,38 +11,39 @@ Base = declarative_base()
 
 
 class Category(Base):
-	__tablename__ = 'category'
-	
-	id = Column(Integer, primary_key=True)
-	name = Column(String(75), nullable=False)
-	
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(75), nullable=False)
+
 
 class Item(Base):
-	__tablename__ = 'item'
-	
-	id = Column(Integer, primary_key=True)
-	name = Column(String(100), nullable=False)
-	description = Column(String(1000))
-	category_id = Column(Integer, ForeignKey('category.id'))
-	category = relationship(Category)
-	photo_path = Column(String(500))
-	user_id = Column(Integer, ForeignKey('user.id'))
-	
-	@property
-	def serialize(self):
+    __tablename__ = 'item'
 
-		return {
-			'name': self.name,
-			'description': self.description,
-			'id': self.id,
-			'category': self.category.name,
-		}
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(1000))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    photo_path = Column(String(500))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    @property
+    def serialize(self):
+
+        return {
+                'name': self.name,
+                'description': self.description,
+                'id': self.id,
+                'category': self.category.name,
+               }
+
 
 class User(Base):
-	__tablename__ = 'user'
-	id = Column(Integer, primary_key=True)
-	name = Column(String(200), nullable=False)
-	email = Column(String(200), nullable=False)
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
+    email = Column(String(200), nullable=False)
 
 
 config = ConfigParser.RawConfigParser()
